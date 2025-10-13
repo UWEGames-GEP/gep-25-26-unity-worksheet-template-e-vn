70fe102e7103861230794e67e90323b4ff5410d5
Evan <Isabelle2.Leveson@live.uwe.ac.uk>
2025-10-13 12:45:22 +0100

    Worksheet Start
    Added script, monoscript, enum and game states

---
+++ Assets/Scripts/Game Manager Add.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class GameManagerAdd : MonoBehaviour
+{
+    public enum GameState
+    {
+        GAMEPLAY,
+        PAUSE
+    }
+
+    public GameState state;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        state = GameState.GAMEPLAY;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Checking current gamestate
+        if (state == GameState.GAMEPLAY)
+        {
+            //Toggle state over return key
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                state = GameState.PAUSE;
+            }
+            else if (state == GameState.PAUSE)
+            {
+                //Toggle state over the return key
+                if (Input.GetKeyDown(KeyCode.Escape))
+                {
+                    state = GameState.GAMEPLAY;
+                }
+
+            }
+
+
+
+
+
+        }
+    }
+
+
+}
