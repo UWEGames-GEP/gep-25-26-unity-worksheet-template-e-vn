fff499d3cd1ac6aa3a11993fd40669bbe3631a36
Evan Leveson <Evan2.Leveson@live.uwe.ac.uk>
2025-11-30 17:42:51 +0000

    Worksheet 1 complete
    worksheet 1 complete, untested

--- Assets/Scripts/Inventory Script.cs
+++ Assets/Scripts/Inventory Script.cs
@@ -5,7 +5,7 @@ public class InventoryScript : MonoBehaviour
 {
     public List<string> items = new List<string>();
     public GameManagerAdd gameManager;
-   
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,24 +16,37 @@ public class InventoryScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-        if (Input.GetKeyDown(KeyCode.T))
-        {
-            AddItem("Generic Item");
-        }
-        if (Input.GetKeyDown(KeyCode.Y))
-        {
-            RemoveItem("Generic Item");
-        }
+      // if (Input.GetKeyDown(KeyCode.T))
+      // {
+      //    AddItem("Generic Item");
+      // }
+      // if (Input.GetKeyDown(KeyCode.Y))
+      // {
+      //    RemoveItem("Generic Item");
+      //  }
     }
 
     public void AddItem(string item)
     {
-        items.Add(item);  
+        items.Add(item);
     }
     public void RemoveItem(string item)
     {
         items.Remove(item);
     }
 
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        //Get Item componenet from hit object if it exisits
+        ItemScript collisionItem = hit.gameObject.GetComponent<ItemScript>();
+
+        //check if object has Item componenet
+        if (collisionItem != null)
+        {
+            //Add the item to data structure
+            items.Add(collisionItem.name);
+        }
+        //Destory game object that item component 
+        Destroy(collisionItem.gameObject);
+    }
 }
